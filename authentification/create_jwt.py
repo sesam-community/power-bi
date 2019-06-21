02064aa0195c1a8e7390ec813734361d532b8ac9
Jonas Als Christensen <jonas.christensen@M-C02TP0J9HF1T.local>
2019-06-21 14:58:13 +0200

    adding integration for azure

--- authentification/create_jwt.py
+++ authentification/create_jwt.py
@@ -1,4 +1,23 @@
 import requests
+import adal
+
+def authenticate_client_key():
+    """
+    Authenticate using service principal w/ key.
+    """
+    authority_host_uri = 'https://login.microsoftonline.com'
+    tenant = '<TENANT>'
+    authority_uri = authority_host_uri + '/' + tenant
+    resource_uri = 'https://management.core.windows.net/'
+    client_id = '<CLIENT_ID>'
+    client_secret = '<CLIENT_SECRET>'
+
+    context = adal.AuthenticationContext(authority_uri, api_version=None)
+    mgmt_token = context.acquire_token_with_client_credentials(resource_uri, client_id, client_secret)
+    credentials = AADTokenCredentials(mgmt_token, client_id)
+
+    return credentials
+
 
 def get_access_token(application_id, application_secret, user_id, user_password):
     data = {
