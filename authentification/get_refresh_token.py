7c0a990fde21d274d66d9344e09612dfa2f66433
Ioannis Skoulis <ioannis.skoulis@sesam.io>
2019-08-01 15:39:05 +0200

    Add script to get a refresh token from device code login

---
+++ authentification/get_refresh_token.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import adal
+import click
+
+
+@click.command()
+@click.option('--auth_endpoint', default='https://login.microsoftonline.com', required=False)
+@click.option('--tenant_id', required=True)
+@click.option('--client_id', required=True)
+@click.option('--resource', required=True)
+def get_refresh_token(auth_endpoint, tenant_id, client_id, resource):
+    context = adal.AuthenticationContext('/'.join([auth_endpoint, tenant_id]))
+    user_code_info = context.acquire_user_code(resource, client_id);
+    print(user_code_info.get('message'))
+    res = context.acquire_token_with_device_code(resource, user_code_info, client_id)
+    print(res.get('refreshToken'))
+
+
+if __name__ == '__main__':
+    get_refresh_token()
