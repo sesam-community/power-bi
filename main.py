fcaa8c448cc9069179cc73e69e47a21136498e26
erik-leven <erik.leven@sesam.io>
2019-08-06 08:06:12 +0200

    changed all the post, get and delete to different routes

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from flask import Flask, request, jsonify
 import json
 from authentification.create_jwt import get_token
 from authentification.auth_helpers import *
-from sesam.getting_sesam_data import get_sesam_data
 from processing.powerBi import *
 
 app = Flask(__name__)
 powerbi_url = "https://api.powerbi.com/v1.0/myorg/datasets"
 sesam_url   = "https://%s.sesam.cloud/api/pipes/%s" %(start_endpoint, pipe_name)
+
+token           = get_token(client_id, tenant_id,refresh_token)
+Powerbi_headers = {'Authorization': "Bearer {}".format(token['accessToken'])}
+Sesam_headers     = {'Authorization': "Bearer {}".format(sesam_jwt)}
 
 @app.route('/')
 def index():
@@ -17,53 +20,52 @@ def index():
     }
     return jsonify(output)
 
-@app.route('/get_data', methods=['GET'])
-def getting_data():
-    token       = get_token(client_id, tenant_id, refresh_token)
-    headers     = {'Authorization': "Bearer {}".format(token['accessToken'])}
-    response    = requests.get(url_dataset, headers=headers)
-    Sesam_data  = get_sesam_data(sesam_jwt, start_endpoint, pipe_name).json()
-    return jsonify(Sesam_data)
 
-@app.route('/post_data', methods=['GET','POST','DELETE'])
+@app.route('/get_powerbi', methods=['GET'])
+def get_powerbi(dataset_id = str()):
+    response    = requests.get(powerbi_url + dataset_id, headers=Powerbi_headers)
+    print(powerbi_url + dataset_id)
+    return response
+
+@app.route('/get_sesam', methods=['GET'])
+def get_sesam(entities = str()):
+    response    = requests.get(sesam_url + entities, headers=Sesam_headers)
+    return response
+
+@app.route('/delete_powerbi', methods=['DELETE'])
+def delete_powerbi(dataset_id):
+    requests.delete(powerbi_url + '/' + dataset_id, headers=Powerbi_headers)
+
+@app.route('/post_data', methods=['POST'])
 def posting_data():
 
-    token           = get_token(client_id, tenant_id,refresh_token)
-    powerbi_headers = {'Authorization': "Bearer {}".format(token['accessToken'])}
-
-
     # Fetching the Sesam data
-    sesam_headers   = {'Authorization': "Bearer {}".format(sesam_jwt)}
-    Sesam_data      = requests.get(sesam_url,headers=sesam_headers).json()
-    entities        = requests.get(sesam_url + "/entities",headers=sesam_headers).json()
+    Sesam_data      = get_sesam().json()
+    entities        = get_sesam('/entities').json()
 
     # Setting up the Power BI table in the correct format
-    table           = setup_table(Sesam_data)
+    table           = setup_table(Sesam_data['_id'])
      
     # Populating the columns (properties)
     populated_table = add_columns(table, entities)
 
     # If the dataset from Sesam already exists in Power BI, 
     # the Power BI dataset gets deleted and replaced by the new one
-    current_datasets            = requests.get(powerbi_url, headers=powerbi_headers)
-
+    current_datasets            = get_powerbi()
     dataset_exists, dataset_id  = check_dataset(current_datasets.json(), populated_table['name'])
     if dataset_exists:
-      requests.delete(powerbi_url + "/" + dataset_id, headers=powerbi_headers)
-    
-    # Adding table with columns in Power BI
-    response        = requests.post(powerbi_url, headers=powerbi_headers, json=populated_table)
+      delete_powerbi(dataset_id)
+    response        = requests.post(powerbi_url, headers=Powerbi_headers, json=populated_table)
     dataset_id      = response.json()['id']
     
     #Populating the rows (entities)
     rows            = add_rows(entities)
     
     # Adding rows to table in Power BI
-    response        = requests.post(powerbi_url + "/%s/tables/%s/rows" % (dataset_id, populated_table['name']), headers=powerbi_headers, json=rows)
+    requests.post(powerbi_url + "/%s/tables/%s/rows" % (dataset_id, populated_table['name']), headers=Powerbi_headers, json=rows)
 
-    response        = requests.get(powerbi_url + "/%s/tables" % dataset_id, headers=powerbi_headers)
-
-    return "done"#jsonify(response.json())
+    response        = get_powerbi('/' + dataset_id)
+    return jsonify(response.json())
 
 
 if __name__ == '__main__':
