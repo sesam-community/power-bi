56b03039fac5ca0115a3e2f78363b6e47d6ff73c
erik-leven <erik.leven@sesam.io>
2019-08-08 10:09:18 +0200

    fixed datetime problems. Works on the first 8 entities in test-pipe

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import json
 from authentification.create_jwt import get_token
 from authentification.auth_helpers import *
 from processing.powerBi import *
+import sys
 
-app = Flask(__name__)
-powerbi_url = "https://api.powerbi.com/v1.0/myorg/groups/07852248-6e81-49b3-b0ac-8c9e9d8c881f/datasets"
-sesam_url   = "https://%s.sesam.cloud/api/pipes/%s" %(start_endpoint, pipe_name)
+app           = Flask(__name__)
+powerbi_url   = "https://api.powerbi.com/v1.0/myorg/groups/07852248-6e81-49b3-b0ac-8c9e9d8c881f/datasets"
+sesam_url     = "https://%s.sesam.cloud/api/pipes/%s" %(start_endpoint, pipe_name)
 
 token           = get_token(client_id, tenant_id,refresh_token)
 Powerbi_headers = {'Authorization': "Bearer {}".format(token['accessToken'])}
-Sesam_headers     = {'Authorization': "Bearer {}".format(sesam_jwt)}
+Sesam_headers   = {'Authorization': "Bearer {}".format(sesam_jwt)}
+
+
 
 @app.route('/')
 def index():
@@ -27,44 +30,74 @@ def get_powerbi(dataset_id = str()):
     return response
 
 @app.route('/get_sesam', methods=['GET'])
-def get_sesam(entities = str()):
-    response    = requests.get(sesam_url + entities, headers=Sesam_headers)
+def get_sesam(endpoint = str()):
+    response    = requests.get(sesam_url + endpoint, headers=Sesam_headers)
     return response
 
-@app.route('/delete_powerbi', methods=['DELETE'])
-def delete_powerbi(dataset_id, table_name):
+@app.route('/update_powerbi_rows', methods=['DELETE'])
+def delete_powerbi_rows(dataset_id, table_name):
     requests.delete(powerbi_url + '/' + dataset_id + '/tables/%s/rows' %table_name, headers=Powerbi_headers)
+
+@app.route('/update_powerbi_columns', methods=['DELETE'])
+def delete_powerbi_columns(dataset_id):
+    requests.delete(powerbi_url + '/' + dataset_id, headers=Powerbi_headers)
+    
+def delete_all_datasets(response):
+  response = get_powerbi()
+  for data in response.json()['value']:
+    delete_powerbi(data['id'])
+
 
 @app.route('/post_data', methods=['GET', 'POST'])
 def posting_data():
+    update_rows    = False
+    update_columns = False
+    new_dataset    = False
 
     # Fetching the Sesam data
-    Sesam_data      = get_sesam().json()
-    entities        = get_sesam('/entities').json()
+    #schema = [{'type': 'string', 'max_size': 9, 'min_size': 0, 'source_property': 'power-bi-embedded:global-company:company-id', 'name': 'power-bi-embedded:global-company:company-id'}, {'type': 'string', 'max_size': 10, 'min_size': 0, 'source_property': 'power-bi-embedded:global-company:name', 'name': 'power-bi-embedded:global-company:name', 'allow_null': True}, {'type': 'string', 'max_size': 8, 'min_size': 0, 'source_property': 'power-bi-embedded:global-company:phone', 'name': 'power-bi-embedded:global-company:phone'}, {'type': 'decimal', 'precision': 4, 'scale': 3, 'max_value': '~f9.812', 'min_value': '~f9.812', 'source_property': 'power-bi-embedded:global-invoice-detail:currency-value', 'name': 'power-bi-embedded:global-invoice-detail:currency-value'}, {'type': 'boolean', 'source_property': 'power-bi-embedded:global-invoice-detail:internal', 'name': 'power-bi-embedded:global-invoice-detail:internal', 'allow_null': True}, {'type': 'boolean', 'source_property': 'power-bi-embedded:global-invoice-detail:invoice', 'name': 'power-bi-embedded:global-invoice-detail:invoice'}, {'type': 'datetime', 'max_value': '~t2019-08-01T00:00:00Z', 'min_value': '~t2019-08-01T00:00:00Z', 'source_property': 'power-bi-embedded:global-invoice-detail:invoice-date', 'name': 'power-bi-embedded:global-invoice-detail:invoice-date'}, {'type': 'string', 'max_size': 58, 'min_size': 0, 'index': True, 'source_property': 'power-bi-embedded:global-invoice-detail:invoice-ni', 'name': 'power-bi-embedded:global-invoice-detail:invoice-ni'}, {'type': 'boolean', 'source_property': 'power-bi-embedded:global-invoice-detail:invoiced', 'name': 'power-bi-embedded:global-invoice-detail:invoiced'}, {'type': 'decimal', 'precision': 4, 'scale': 1, 'max_value': '~f300.0', 'min_value': '~f300.0', 'source_property': 'power-bi-embedded:global-invoice-detail:max', 'name': 'power-bi-embedded:global-invoice-detail:max'}, {'type': 'string', 'max_size': 21, 'min_size': 0, 'source_property': 'power-bi-embedded:global-invoice-detail:name', 'name': 'power-bi-embedded:global-invoice-detail:name'}, {'type': 'string', 'max_size': 20, 'min_size': 0, 'index': True, 'source_property': 'power-bi-embedded:global-invoice-detail:period-ni', 'name': 'power-bi-embedded:global-invoice-detail:period-ni'}, {'type': 'string', 'max_size': 24, 'min_size': 0, 'index': True, 'source_property': 'power-bi-embedded:global-invoice-detail:product-ni', 'name': 'power-bi-embedded:global-invoice-detail:product-ni'}, {'type': 'decimal', 'precision': 3, 'scale': 1, 'max_value': '~f31.0', 'min_value': '~f31.0', 'source_property': 'power-bi-embedded:global-invoice-detail:quantity', 'name': 'power-bi-embedded:global-invoice-detail:quantity'}, {'type': 'decimal', 'precision': 9, 'scale': 5, 'max_value': '~f4562.58000', 'min_value': '~f4562.58000', 'source_property': 'power-bi-embedded:global-invoice-detail:total-price', 'name': 'power-bi-embedded:global-invoice-detail:total-price'}, {'type': 'string', 'max_size': 12, 'min_size': 0, 'source_property': 'power-bi-embedded:global-invoice-detail:unit', 'name': 'power-bi-embedded:global-invoice-detail:unit', 'allow_null': True}, {'type': 'decimal', 'precision': 3, 'scale': 1, 'max_value': '~f15.0', 'min_value': '~f15.0', 'source_property': 'power-bi-embedded:global-invoice-detail:unit-price', 'name': 'power-bi-embedded:global-invoice-detail:unit-price'}, {'type': 'boolean', 'source_property': 'power-bi-embedded:global-paymentmethod:billable', 'name': 'power-bi-embedded:global-paymentmethod:billable', 'allow_null': True}, {'type': 'string', 'max_size': 26, 'min_size': 0, 'index': True, 'source_property': 'power-bi-embedded:global-paymentmethod:company-ni', 'name': 'power-bi-embedded:global-paymentmethod:company-ni'}, {'type': 'string', 'max_size': 3, 'min_size': 0, 'source_property': 'power-bi-embedded:global-paymentmethod:currency', 'name': 'power-bi-embedded:global-paymentmethod:currency'}, {'type': 'boolean', 'source_property': 'power-bi-embedded:global-paymentmethod:internal', 'name': 'power-bi-embedded:global-paymentmethod:internal'}, {'type': 'string', 'max_size': 24, 'min_size': 0, 'source_property': 'power-bi-embedded:global-paymentmethod:name', 'name': 'power-bi-embedded:global-paymentmethod:name'}, {'type': 'datetime', 'max_value': '~t2019-08-31T23:59:59Z', 'min_value': '~t2019-08-31T23:59:59Z', 'source_property': 'power-bi-embedded:global-period:end', 'name': 'power-bi-embedded:global-period:end'}, {'type': 'string', 'max_size': 10, 'min_size': 0, 'source_property': 'power-bi-embedded:global-period:resource-date', 'name': 'power-bi-embedded:global-period:resource-date', 'allow_null': True}, {'type': 'decimal', 'precision': 4, 'scale': 1, 'max_value': '~f300.0', 'min_value': '~f300.0', 'source_property': 'power-bi-embedded:global-product:rate', 'name': 'power-bi-embedded:global-product:rate'}]
+    
+    #response = get_sesam("/entities")#?since=1&limit=8")
+    response = get_sesam("/entities?since=1&limit=8")
+    if response.status_code != 200:
+        print(response)
+        print('bad request fo Sesam')
+        sys.exit()
+    else:
+        entities = response.json()
+
+    schema = [{'type': 'string', 'max_size': 23, 'min_size': 8, 'allow_null': True, 'source_property': 'global-company:name', 'name': 'global-company:name'}, {'type': 'decimal', 'precision': 5, 'scale': 4, 'max_value': '~f9.9483', 'min_value': '~f9.4125', 'source_property': 'global-invoice-detail:currency-value', 'name': 'global-invoice-detail:currency-value'}, {'type': 'datetime', 'max_value': '~t2019-04-01T00:00:00Z', 'min_value': '~t2017-10-01T00:00:00Z', 'source_property': 'global-invoice-detail:invoice-date', 'name': 'global-invoice-detail:invoice-date'}, {'type': 'decimal', 'precision': 12, 'scale': 6, 'max_value': '~f102899.161600', 'min_value': '~f0.00000', 'source_property': 'global-invoice-detail:total-price', 'name': 'global-invoice-detail:total-price'}, {'type': 'decimal', 'precision': 5, 'scale': 1, 'max_value': '~f1500.0', 'min_value': '~f0.0', 'source_property': 'global-invoice-detail:unit-price', 'name': 'global-invoice-detail:unit-price'}, {'type': 'boolean', 'allow_null': True, 'source_property': 'global-paymentmethod:billable', 'name': 'global-paymentmethod:billable'}]
+
+    #ss
+    #schema          = get_sesam("/generate-schema-definition").json()
+    #print(schema)
+    #ss
 
     # Setting up the Power BI table in the correct format
-    table           = setup_table(Sesam_data['_id'])
-     
-    # Populating the columns (properties)
-    populated_table = add_columns(table, entities)
+    dataset           = setup_dataset(pipe_name)
+    populated_dataset, keys = add_columns(dataset, entities, schema)
+    rows            = add_rows(entities, populated_dataset, keys)
 
     # If the dataset from Sesam already exists in Power BI, 
-    # the Power BI dataset gets deleted and replaced by the new one
+    # the Power BI dataset gets updated
     
     current_datasets            = get_powerbi()
-    dataset_exists, dataset_id  = check_dataset(current_datasets.json(), populated_table['name'])
-    if dataset_exists:
-      delete_powerbi(dataset_id, populated_table['name'])
-    
-    #response        = requests.post(powerbi_url, headers=Powerbi_headers, json=populated_table)
-    #dataset_id      = response.json()['id']
-    
-    #Populating the rows (entities)
-    rows            = add_rows(entities)
-    # Adding rows to table in Power BI
-    requests.post(powerbi_url + "/%s/tables/%s/rows" % (dataset_id, populated_table['name']), headers=Powerbi_headers, json=rows)
+    update_rows, update_columns, new_dataset, dataset_id = check_dataset_status(current_datasets, entities, pipe_name, update_rows, update_columns, new_dataset)
+    if not update_rows:
+      if update_columns:
+        delete_powerbi_columns(dataset_id)
+        requests.post(powerbi_url, headers=Powerbi_headers, json=populated_dataset)
+      else:
+        requests.post(powerbi_url, headers=Powerbi_headers, json=populated_dataset)
+      response = get_powerbi()
+      dataset_id = find_dataset_id(response, pipe_name)
+    else:
+      delete_powerbi_rows(dataset_id, pipe_name)
 
+    # Populating the rows in the dataset
+    requests.post(powerbi_url + "/%s/tables/%s/rows" % (dataset_id, pipe_name), headers=Powerbi_headers, json=rows)
     response        = get_powerbi('/' + dataset_id)
+    
     return jsonify(response.json())
 
 
--- processing/powerBi.py
+++ processing/powerBi.py
@@ -1,8 +1,11 @@
 import json
 from six import iteritems
 import requests
+import sys
 
-def setup_table(Sesam_data):
+
+#tables = [Sesam_data_id, "dates"]
+def setup_dataset(Sesam_data_id):
     """ 
     Creates a new dictionary with the standard Power BI setup
     with one dataset and one table 
@@ -14,16 +17,54 @@ def setup_table(Sesam_data):
         A new dictionary for Power BI use
     """
 
-    new_table                = {}
-    new_table['name']        = Sesam_data['_id']
-    new_table["defaultMode"] = "Push"
-    new_table['tables']      = []
+    tables = [Sesam_data_id]
+    new_dataset                = {}
+    new_dataset['name']        = Sesam_data_id
+    new_dataset["defaultMode"] = "Push"
+    new_dataset['tables']      = []
 
-    new_table['tables'].append({})
-    new_table['tables'][0]['name']    = Sesam_data['_id'] 
-    new_table['tables'][0]['columns'] = []
-    return new_table
+    for i, table in enumerate(tables):
+        new_dataset['tables'].append({})
+        new_dataset['tables'][i]['name']    = table 
+        new_dataset['tables'][i]['columns'] = []
 
+    return new_dataset
+
+def find_dataset_id(response, pipe_name):
+    for dataset in response.json()['value']:
+        print(dataset)
+        if dataset['name'] == pipe_name:
+            return dataset['id']
+    print("No matching dataset was found")
+    sys.exit()
+
+def check_dataset_status(current_datasets, entities, pipe_name, update_rows, update_columns, new_dataset):
+    try:
+      num_keys_new = len(entities[0].keys())
+    except IndexError:
+      print("There are no entities in the Sesam data")
+      sys.exit()
+
+    try:
+      current_datasets.json()['value']
+      for dataset in current_datasets.json()['value']:
+        if dataset['name'] == pipe_name:
+          dataset_id = dataset['id'] 
+          num_keys_old = len(current_datasets.json()['value'][0].keys())
+          if num_keys_old == num_keys_new:
+            update_rows = True
+            break
+          else:
+            update_columns = True
+            break
+      if not update_columns and not update_rows:
+        dataset_id = False
+        new_dataset = True
+    except KeyError:
+      new_dataset = True
+      dataset_id = False
+
+    return update_rows, update_columns, new_dataset, dataset_id
 
 def check_dataset(current_datasets, new_dataset_name):
     """
@@ -43,7 +84,6 @@ def check_dataset(current_datasets, new_dataset_name):
         if current_datasets['name'] == new_dataset_name:
             return True, current_datasets['id']
         else:
-            print(1)
             return False, None
 
     if len(current_datasets['value']) == 0:
@@ -53,11 +93,10 @@ def check_dataset(current_datasets, new_dataset_name):
         if existing_dataset['name'] == new_dataset_name:
             return True, existing_dataset['id']
         else:
-            print(3)
-
             return False, None
 
-def add_columns(new_dict, entities):
+
+def add_columns(new_dict, entities, schema):
     """
     Fills in the columns in the disctionary from setup_powerBi_json
     with Sesam entities as rows, and values as columns 
@@ -69,69 +108,75 @@ def add_columns(new_dict, entities):
     Returns:
         A dictionary with the Sesam data attached in Power BI format
     """
-    num_cols = len(entities[0].keys())
-    col_keys = list(entities[0].keys())
-    # TODO check if all entities has same amount of keys
 
-    for col in range(num_cols):
-        new_dict['tables'][0]['columns'].append({})                                                       
-        new_dict['tables'][0]['columns'][col]['name'] = col_keys[col]                                      
-        new_dict['tables'][0]['columns'][col]['dataType'] = find_dataType(col_keys[col], entities[0][col_keys[col]])
-    return new_dict
+    num_tables = len(new_dict['tables'])
+    num_columns = len(schema)
 
-def add_rows(entities):
+    keys = [data['name'] for data in schema]
+    for i in range(num_tables):
+        for j in range(num_columns):
+            try:
+                dataType = schema[j]['type'].capitalize()
+                if dataType == 'Null':
+                    dataType = 'String'
+
+            except IndexError:
+                dataType = 'String'
+
+            new_dict['tables'][i]['columns'].append({})                                                       
+            new_dict['tables'][i]['columns'][j]['name'] = keys[j]
+            if dataType == 'Datetime':
+                new_dict['tables'][i]['columns'][j]['dataType'] = 'DateTime'
+                new_dict['tables'][i]['columns'][j]['formatString'] = "m/d/yyyy"
+                #new_dict['tables'][0]['columns'][col]['summarizeBy'] = "none"
+            else:
+                new_dict['tables'][i]['columns'][j]['dataType'] = dataType
+    return new_dict, keys
+
+def add_rows(entities, populated_dataset, keys):
+
     rows         = {}
     rows["rows"] = []
-    num_cols     = len(entities)
-    for i in range(len(entities)):
-        rows["rows"].append({})
-        for j, key in enumerate(entities[i].keys()):
-            rows["rows"][i][key] = entities[i][key]
-        
+    num_rows     = len(entities)
+    num_tables   = len(populated_dataset['tables'])
+
+    for i in range(num_tables):
+        for j, entity in enumerate(entities):
+            rows["rows"].append({})
+            for k, key in enumerate(keys):
+                dataType = populated_dataset['tables'][i]['columns'][k]['dataType']
+                try:
+                    value = format_value(entity[key], dataType)
+                except KeyError:
+                    value = format_value("", dataType)
+                rows['rows'][j][key] = value
     return rows
 
-def find_dataType(key, value):
-    """
-    Method for sorting out the most obvious dataTypes for specification in Power BI
+def format_value(value, dataType):
+    if dataType == 'Boolean':
+        value = str(value).capitalize()
+        if value != 'False' and value != 'True':
+            value = 'False' # default is missing value
+        value = bool(value)
+        return value
 
-    Parameters:
-        key:    The key used from the dictionary
-        value:  The value associated with the key  
+    if dataType == 'DateTime':
+        print(value)
+        split_value = value.split()
+        if len(split_value) == 0:
+            return "0000-00-00"
 
-    Returns: 
-        A string for dataType specification 
-    """
-    
+        if split_value[0][0] == '~':
+            return value.split()[0][2:]
+        else:
+            return value
 
-    key = key.lower()
-    if key == 'ssn':
-        return 'Int64'
-        
-    if key == 'datetime':
-        return 'dateTime' 
-
-    if key == 'address' or key == 'adresse':
-        return 'String'
-
-    if len(value.split()) != 1:
-        return 'String'
-
-    if value.lower() == 'true' or value.lower() == 'false':
-        return 'Boolean'
-    try:
-        eval(value)
-    except NameError:
-        return 'String'
-    except SyntaxError:
-        return 'String'
-    except TypeError:
-        return 'String'
-
-    if type(eval(value)) == int:
-        return 'Int64'
-
-    if type(eval(value)) == float:
-        # TODO: Always return Decimal for float atm. Seems like the safest option rn
-        return 'Decimal'
-
-    return 'Datetime'
+    if dataType == 'Decimal':
+        if value == "":
+            return "0.0"
+        if str(value).split()[0][0] == '~':
+            return str(value).split()[0][2:]
+        else:
+            return str(value)
+    else:
+        return str(value)
